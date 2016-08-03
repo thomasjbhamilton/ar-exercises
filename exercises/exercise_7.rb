d4b1435e75cadd4caa0d65683cf3c2cfa86e754a
thomasjbhamilton <thomas@reverbconsulting.com>
2016-08-03 15:20:07 -0400

    update employee, store and exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,7 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+
+puts "What store are you looking for?"
+answer = gets.chomp
+bobs = Store.create!(name: answer).save
